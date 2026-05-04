98acc7195b08de83b7bed35c86bd64179c4d1b5c
lyminea2422-stack <niron.camdoc@gmail.com>
2026-05-04 09:16:56 +0700

    fix-backend-api

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    host: true,   // exposes local IP + localhost
+    port: 5173    // you can change the port if needed
+  }
 })
