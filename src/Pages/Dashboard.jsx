139e8e77ab59128f485efb1a3819a27cb18ddd6a
amrin2509 <sorcheatamrin954@gmail.com>
2026-05-18 19:07:43 +0700

    translate

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -604,7 +604,7 @@ export default function Dashboard() {
                   "&:hover": { bgcolor: theme.palette.action.hover, borderColor: theme.palette.primary.main },
                 }}
               >
-                Export
+                {t("export")}
               </Button>
               <Button
                 onClick={handlePrint}
